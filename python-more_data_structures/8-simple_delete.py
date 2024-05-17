b214e26a4139f7b79df8d0ca12396edfe28e9987
Neia2 <nenaslife@gmail.com>
2024-05-17 09:08:26 +0200

    updating 5, answer 7 and 8

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    """
+    Deletes a key in a dictionary.
+
+    :param a_dictionary: The dictionary from which to delete the key
+    :param key: The key to delete (always a string)
+    :return: The updated dictionary
+    """
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
