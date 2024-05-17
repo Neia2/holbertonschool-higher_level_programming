875232d43509ac4ca9e7dfd333ea34691e70abb2
Neia2 <nenaslife@gmail.com>
2024-05-17 10:28:02 +0200

    fixing return statement 5, 9, 10 and 11 answers

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    """
+    Returns a new dictionary with all values multiplied by 2.
+
+    :param a_dictionary: The original dictionary with integer values
+    :return: A new dictionary with values multiplied by 2
+    """
+    new_dict = {key: value * 2 for key, value in a_dictionary.items()}
+    return new_dict
