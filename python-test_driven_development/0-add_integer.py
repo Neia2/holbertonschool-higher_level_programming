ece0ee6b4ccd1f16fa1289fb9cf041f1cd7babc7
Neia2 <nenaslife@gmail.com>
2024-05-22 12:33:43 +0200

    fixing task

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    """Adds two integers or integers casted from floats.
-
+    """returns sum of a & b
     Args:
-        a: The first value, must be an integer or float.
-        b: The second value, must be an integer or float, default is 98.
-
-    Returns:
-        The integer sum of a and b.
-
-    Raises:
-        TypeError: If either a or b is not an integer or float.
+        a: int
+        b: int
     """
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
 
-    a = int(a)
-    b = int(b)
-    
-    return a + b
+    if not isinstance(a, (int, float)):
+        raise TypeError('a must be an integer')
+    elif not isinstance(b, (int, float)):
+        raise TypeError('b must be an integer')
+    else:
+        return(int(a) + int(b))
