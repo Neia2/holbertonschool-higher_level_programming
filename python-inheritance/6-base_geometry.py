341421419ac64e99871ce04c5fdefc3248c03908
Neia2 <nenaslife@gmail.com>
2024-05-26 21:26:41 +0200

    files and answers

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """Base geometry class"""
+
+    def area(self):
+        """area function that is empty"""
+        raise Exception('area() is not implemented')
