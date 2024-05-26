05725bdb5778f6bc2b4adbdb39d002f894c907d9
Neia2 <nenaslife@gmail.com>
2024-05-26 22:15:15 +0200

    updating 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
+"""
+Module: 10-square
+
+This module defines a class Square, which inherits from Rectangle.
+
+"""
+
 Rectangle = __import__('9-rectangle').Rectangle
 
-
 class Square(Rectangle):
-    """square class"""
+    """A class representing a square, a special case of rectangle with equal sides."""
 
     def __init__(self, size):
-        """initalizer
-        Args
-           size: size of side of square
+        """Initializes a square with a given size.
+
+        Args:
+            size (int): The size of one side of the square.
         """
         super().integer_validator('size', size)
         Rectangle.__init__(self, size, size)
         self.__size = size
 
     def __str__(self):
-        """returns dimension of square"""
-        return('[Square] {}/{}'.format(self.__size, self.__size))
+        """Returns a string representation of the square's dimensions."""
+        return '[Square] {}/{}'.format(self.__size, self.__size)
