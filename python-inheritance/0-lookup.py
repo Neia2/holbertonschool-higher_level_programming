9d33158cd235189b678bf558ddd97be13d417745
Neia2 <nenaslife@gmail.com>
2024-05-26 22:04:32 +0200

    doc

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""
+Module: 0-lookup
 
+This module provides a function to retrieve the list of attributes and methods of an object.
+
+"""
 
 def lookup(obj):
-    """ returns list of available attributes and methods for object"""
-    return(dir(obj))
+    """Returns the list of attributes and methods of an object.
+
+    Args:
+        obj: The object to inspect.
+
+    Returns:
+        list: A list containing the names of attributes and methods of the object.
+    """
+    return dir(obj)
--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
+
+"""
+Module: 1-my_list
+
+This module defines a class MyList, which is a subclass of the built-in list class.
+
+"""
+
 class MyList(list):
-    """class utilizing list class"""
+    """A custom list class that extends the built-in list."""
 
     def __init__(self):
-        """initializer for MyList"""
+        """Initializes a MyList object."""
         pass
 
     def print_sorted(self):
-        """print sorted list"""
-        res = list.copy(self)
-        list.sort(res)
-        print(res)
+        """Prints the list sorted."""
+        sorted_list = sorted(self)
+        print(sorted_list)
