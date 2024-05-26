3d372db73742eccba68ee5f488ded3c80a2e7bdc
Neia2 <nenaslife@gmail.com>
2024-05-26 21:42:47 +0200

    fixing

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
+
+"""
+Module: 2-is_same_class
+
+This module defines a function is_same_class that checks if an object is exactly an instance
+of a specified class.
+
+"""
+
 def is_same_class(obj, a_class):
     """checks if object is exactly an instance of a_class
     Args
        obj: object
        a_class: class
     """
-    return(type(obj) == a_class)
+    return type(obj) == a_class
--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+
+"""
+Module: 3-is_kind_of_class
+
+This module defines a function is_kind_of_class that checks if an object is an instance of a class
+inherited from a specified class.
+
+"""
+
 def is_kind_of_class(obj, a_class):
     """checks if obj is an instance of a class inherited from a_class
     Args
@@ -6,5 +15,5 @@ def is_kind_of_class(obj, a_class):
        a_class: class
     Return: True or False"""
     if isinstance(obj, a_class):
-        return(True)
-    return(False)
+        return True
+    return False
