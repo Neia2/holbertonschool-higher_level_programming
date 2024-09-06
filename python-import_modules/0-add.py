e4ef43fd173dd660625924b36807b1d92737a12a
Neia2 <7276@holbertonstudents.com>
2024-09-06 22:17:52 +0200

    t0

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module provides a function 'add' that takes two integers
+and returns their sum.
+"""
+
+from add_0 import add
+
+
+a = 1
+b = 2
+
+
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a, b)))
