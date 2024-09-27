5536e8d85345ae9d0bd7f23b768e03979c3d7ed0
Neia2 <7276@holbertonstudents.com>
2024-09-27 15:29:50 +0200

    7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+"""A class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Base class for geometry objects"""
+
+    def area(self):
+        """Public instance method that raises an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Public instance method that validates value"""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
