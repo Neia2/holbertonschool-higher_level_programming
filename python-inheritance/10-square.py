95267524602d2e53dfa3b3edc698d5ba3e5a0ca9
Neia2 <7276@holbertonstudents.com>
2024-09-27 13:40:37 +0200

    T8 - T11 & 7txt

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square that inherits from Rectangle """
+
+    def __init__(self, size):
+        """ Initialize a square with a single size value """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square that inherits from Rectangle """
+
+    def __init__(self, size):
+        """ Initialize a square with a single size """
+        self.integer_validator("size", size)
+        self.__size = size
+
+        super().__init__(size, size)
+
+    def __str__(self):
+        """ Return the square description """
+        return f"[Square] {self.__size}/{self.__size}"
