e34485ec8d7b1be02b74f03a717cb57c80636a51
Neia2 <7276@holbertonstudents.com>
2024-10-04 11:11:28 +0200

    updating docstring

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Reading a file"""
+
+
 def read_file(filename="my_file_0.txt"):
+    """A function that reads a text file and prints it to stdout"""
     with open(filename, 'r', encoding="utf-8") as file:
         print(file.read())
