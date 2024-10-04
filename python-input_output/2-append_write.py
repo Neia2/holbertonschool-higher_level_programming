12f390980ec38c9e3892a765e802c25b19af28fe
Neia2 <7276@holbertonstudents.com>
2024-10-04 15:55:44 +0200

    T2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""A function that writes a string to a text file (UTF8) and returns the
+ number of characters written"""
+
+
+def append_write(filename="", text=""):
+    """appending a text to a file"""
+    with open('file_append.txt', mode="a", encoding="utf-8") as f:
+        return f.write(text)
