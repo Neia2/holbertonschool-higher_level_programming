88fa7a375ceaaf80c15d7a4fa8de153f21b776c0
Neia2 <7276@holbertonstudents.com>
2024-10-04 15:49:53 +0200

    updating docstring

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""A function that writes a string to a text"""
+
+
+def write_file(filename="", text=""):
+    """Write a string to a text file (UTF8)."""
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
