aad43b3876b3d7b2e86b2d536d85cde4052d62ec
Neia2 <7276@holbertonstudents.com>
2024-10-04 11:23:41 +0200

    changing print format

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ A module that contains a function to read and print a text file.
 
 def read_file(filename="my_file_0.txt"):
     """Reads a text file and prints it to stdout"""
-    with open(filename, 'r', encoding="utf-8") as file:
-        print(file.read())
+    with open(filename, 'r', encoding="utf-8") as f:
+        print("{}".format(f.read()), end="")
