d23ad86dfc7c5c47d26440f85e571252794f5944
Neia2 <7276@holbertonstudents.com>
2024-11-17 23:50:59 +0100

    updating tasks

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Create a class named CountedIterator that extends the built-in iterator """
+class CountedIterator:
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)  # Create an iterator from the given iterable
+        self.counter = 0  # Initialize the counter
+
+    def __next__(self):
+        # Increment counter and return the next item
+        self.counter += 1
+        return next(self.iterator)
+
+    def get_count(self):
+        # Return the count of iterated items
+        return self.counter
